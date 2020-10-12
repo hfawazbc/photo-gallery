dd67c26da0b424794871809f385a788dc64f62e5
hfawazbc <hfawaz2520@gmail.com>
2020-10-11 18:28:58 -0700

    Edited header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,13 @@ import React from 'react';
 export default function Header() {
     return (
         <div className="header-container">
-            <h1 style={{ marginTop: '50px', textAlign: 'center', fontSize: '3em', borderBottom: 'solid black 1px' }}>Photo Gallery</h1>
+            <h1 style={{ 
+                marginTop: '50px', 
+                textAlign: 'center', 
+                fontSize: '3em', 
+                borderBottom: 'solid black 1px', 
+                padding: "10px" 
+            }}>Photo Gallery</h1>
         </div>
     )
 }
