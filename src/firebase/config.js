c10b8a1e55ca687da85c9a72f4befb169109175b
hfawazbc <hfawaz2520@gmail.com>
2020-10-17 15:38:27 -0700

    Updated firebase .env variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,14 +4,14 @@ import 'firebase/firestore';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyBBg6rkjAFnouSzn5jKwbBuePq7fvTCRo8",
-    authDomain: "photo-gallery-fd50c.firebaseapp.com",
-    databaseURL: "https://photo-gallery-fd50c.firebaseio.com",
-    projectId: "photo-gallery-fd50c",
-    storageBucket: "photo-gallery-fd50c.appspot.com",
-    messagingSenderId: "861784858401",
-    appId: "1:861784858401:web:17f586dab6a7e5b2d2e860",
-    measurementId: "G-EZC39F94QV"
+    apiKey: REACT_APP_API_KEY,
+    authDomain: REACT_APP_AUTH_DOMAIN,
+    databaseURL: REACT_APP_DATABASE_URL,
+    projectId: REACT_APP_PROJECT_ID,
+    storageBucket: REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+    appId: REACT_APP_APP_ID,
+    measurementId: REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
